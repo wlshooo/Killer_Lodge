d0de24760d35a371ce873fafcf340eb72ba7a4ea
jinhoLee <kswlsgh@naver.com>
2022-11-26 12:23:16 +0900

    computer memo update complete

---
+++ Assets/Script/ActionController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionController : MonoBehaviour
+{
+    [SerializeField]
+    private float range; // 아이템 습득이 가능한 최대 거리
+
+    private bool pickupActivated = false; //아이템 습득 가능할 시 True;
+
+    private RaycastHit hitInfo; //충돌체 정보 저장
+
+    [SerializeField] LayerMask layerMask;  //특정 레이어를 가진 오브젝트에 대해서만 습득할 수 있어야 한다.
+
+    [SerializeField]
+    private Text actionText;    //행동을 보여 줄 텍스트;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckItem();
+        //TryAction();
+    }
+    private void CheckItem()
+    {
+        if(Physics.Raycast(transform.position,transform.forward,out hitInfo , range, layerMask))
+        {
+            if(hitInfo.transform.tag =="Computer")
+            {
+                ItemInfoAppear();
+            }
+            
+        }
+        else
+        {
+            ItemInfoDisappear();
+        }
+    }
+    private void ItemInfoAppear()
+    {
+        pickupActivated = true;
+        actionText.gameObject.SetActive(true);
+        actionText.text = hitInfo.transform.GetComponent<ItemPickUp>().item.itemName + " 메모를 읽을 수 있습니다. " + "<color=yellow>" + "[마우스 좌클릭]" + "</color>";
+    }
+    private void ItemInfoDisappear()
+    {
+        pickupActivated = false;
+        actionText.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Script/Item.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    public string itemName; //아이템의 이름
+    public Sprite itemImage;    //아이템의 이미지
+    public GameObject itemPrefab;   //아이템의 프리팹
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
